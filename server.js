bee9b69f8530297df687079acfbd76800b03a98a
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-03-27 12:08:30 +0300

    added new section

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
+// Use middlewares and router
 server.use(middlewares);
 server.use(router);
+
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
